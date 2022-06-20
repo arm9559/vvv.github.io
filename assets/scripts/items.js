7c522594e13795e605928f3ae2cf2cd1a4a741f4
Armen Vardanyan <arm.vardanyan9559@gmail.com>
2022-06-20 10:58:30 +0400

    commit

---
+++ assets/scripts/items.js
@@ -0,0 +1,27 @@
+function itemOpenOne(){
+        $('.item-inner-one').slideToggle()
+}
+
+function itemOpenTwo(){
+        $('.item-inner-two').slideToggle()
+}
+
+function itemOpenTree(){
+        $('.item-inner-tree').slideToggle()
+}
+
+function itemOpenFour(){
+        $('.item-inner-four').slideToggle()
+}
+
+function itemOpenFive(){
+        $('.item-inner-five').slideToggle()
+}
+
+function itemOpenSix(){
+        $('.item-inner-six').slideToggle()
+}
+
+function itemOpenSeven(){
+        $('.item-inner-seven').slideToggle()
+}
